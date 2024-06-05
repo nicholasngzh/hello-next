b005368b82cb33232e1e01a201d06ccbaef38572
Nicholas Ng <nickng-1998@hotmail.com>
2024-06-06 01:12:05 +0800

    implement server timestamp

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react"
 
-import './styles.css';
+import "./styles.css"
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>Page rendered on {new Date().toLocaleString()}</footer>
       </body>
     </html>
-  );
+  )
 }
 
-export default RootLayout;
+export default RootLayout
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react"
 
 function Home() {
   return (
     <main>
       <h1>Hello Next!</h1>
     </main>
-  );
+  )
 }
 
-export default Home;
+export default Home
